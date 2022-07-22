6432cd8b4abed6f880e664947f5dab2b8a0b9c96
IrynaBuiar <irynasavka5546@gmail.com>
2022-07-22 20:18:52 +0300

    Initial commit

---
+++ lesson1/main.js
@@ -0,0 +1,83 @@
+let a = 'hello';
+console.log(a);
+document.write(a);
+alert(a);
+let b = 'owu';
+console.log(b);
+document.write(b);
+alert(b);
+let c = 'com';
+console.log(c);
+document.write(c);
+alert(c);
+let d = 'ua';
+console.log(d);
+document.write(d);
+alert(d);
+let f = 1;
+console.log(f);
+document.write(f);
+alert(f);
+let e = 10;
+console.log(e);
+document.write(e);
+alert(e);
+let g = -999;
+console.log(g);
+document.write(g);
+alert(g);
+let h = 123;
+console.log(h);
+document.write(h);
+alert(h);
+let i = 2.7;
+console.log(i);
+document.write(i);
+alert(i);
+let j = 16;
+console.log(j);
+document.write(j);
+alert(j)
+let k = true;
+console.log(k);
+document.write(k);
+alert(k);
+let p = false;
+console.log(p);
+document.write(p);
+alert(p)
+console.log('Buiar' !== 'Iryna');
+
+
+let firsName = 'Buiar';
+let middleName = 'Iryna';
+let lastName = 'Ihorivna';
+let person = firsName + middleName + lastName;
+console.log(person);
+let person1 = firsName + ' ' + middleName + ' ' + lastName;
+console.log(person1);
+
+let l = 100;
+console.log(typeof l);
+let m = 100;
+console.log(typeof m);
+let n = true
+console.log(typeof n);
+
+let name = prompt('Ваше ім"я');
+alert('Привіт ' + name + ' !');
+console.log( name);
+let secondname = prompt('Ведіть по-батькові');
+console.log( secondname);
+alert('Вітаємо вас, Ірина ' + secondname + '!');
+let threname = prompt('Скільки, Вам, років');
+console.log(threname);
+alert('Дякую за відповідь ' + threname + ' хороший вік');
+
+
+
+
+
+
+
+
